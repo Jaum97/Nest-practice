c238892b907187957c1d15444c9c5264a1c80820
João Medeiros <33334915+Jaum97@users.noreply.github.com>
2019-10-01 23:46:22 -0300

    create

---
+++ controllers/cats.controller.ts
@@ -0,0 +1,11 @@
+//https://docs.nestjs.com/controllers
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
